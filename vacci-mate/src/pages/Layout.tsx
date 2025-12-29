a847593d9119f8e4c3009ae6db5f7d359ede4a18
TeaGross <tea.grossman@gmail.com>
2025-12-29 10:19:00 +0100

    feat: added Header and Logo components with styled buttons, also added google fonts
    -installed style components
    -added link for google fonts in index.html

--- vacci-mate/src/pages/Layout.tsx
+++ vacci-mate/src/pages/Layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router';
+import { Header } from '../components/Header/Header';
 
 export const Layout = () => {
     return (
         <>
-        <header>Header</header>
+        <Header></Header>
         <main>
             <Outlet></Outlet>
         </main>
