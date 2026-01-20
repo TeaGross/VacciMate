8f0fdfc9b651ac3647bce88fa81127b2b82ccf6e
TeaGross <tea.grossman@gmail.com>
2026-01-20 15:22:30 +0100

    feat: implement email reminders with mock service & emailJS
    - Added EmailJS dependency to package.json for email functionality.
    - Implemented reminder email sending in the new sendReminderEmail service.
    - Created a Reminder model to structure reminder data.
    -Set up MSW (Mock Service Worker) for reminders
    - Developed a reminderService to handle reminder creation and email sending.
    - Updated VaccinationForm to create reminders when updating/adding vaccinations.
    - Added mock handlers for scheduling reminders and sending emails.
    - Implemented date formatting utility for consistent date representation.

---
+++ vacci-mate/src/email/sendReminderEmail.ts
@@ -0,0 +1,23 @@
+import emailjs from '@emailjs/browser';
+import { formatDate } from '../utils/formatDate';
+
+console.log('🔥 sendReminderEmail file loaded');
+
+export function sendReminderEmail(reminder: {
+    email: string;
+    vaccineName: string;
+    remindAt: string;
+    }) {
+    console.log('📨 sendReminderEmail CALLED', reminder);
+
+    return emailjs.send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID!,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID!,
+        {
+        email: reminder.email,
+        vaccine_name: reminder.vaccineName,
+        remind_at: formatDate(reminder.remindAt),
+        },
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY!
+    );
+}
--- vacci-mate/src/main.tsx
+++ vacci-mate/src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import emailjs from '@emailjs/browser';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// Initiera EmailJS
+emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY!);
+
+async function enableMocking() {
+  if (import.meta.env.VITE_ENABLE_MSW === 'true') {
+    const { worker } = await import('./mocks/browser.ts');
+    await worker.start({
+      onUnhandledRequest: 'bypass',
+    });
+  }
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
