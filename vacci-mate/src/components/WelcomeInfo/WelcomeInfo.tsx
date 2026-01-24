78e61f218064b1fd52038689659b40b1970751fb
TeaGross <tea.grossman@gmail.com>
2026-01-24 18:53:48 +0100

    fix: improvements after user tests
    -changed color for check-icons on start page
    -added new clarifying copy for date of vaccination
    -new copy for welcome-info
    -added scrolltotop-funktion
    -new mock-data better suiting user persona used for demo
    -add fixed position for header

--- vacci-mate/src/components/WelcomeInfo/WelcomeInfo.tsx
+++ vacci-mate/src/components/WelcomeInfo/WelcomeInfo.tsx
@@ -10,8 +10,9 @@ export const WelcomeInfo = () => {
             <div className='welcome-info'>
                 <h2>Välkommen {activeUser?.firstName}! </h2>
                 <p>Här hittar du en översikt över dina registrerade vaccinationer. 
-                    Klicka på en vaccination för att se detaljer eller redigera den, 
-                    eller lägg till en ny vaccination för att hålla din historik uppdaterad.</p>
+                    Lägg till en ny vaccination för att hålla din historik uppdaterad eller
+                    klicka på en vaccination för att se detaljer och redigera. 
+                    .</p>
             </div>
         </div>
         </>
